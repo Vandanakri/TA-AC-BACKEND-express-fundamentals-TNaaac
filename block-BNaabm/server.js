385658fe8627c824b898e8a4cf7144a7908e779d
vandana <vandana123annusah@gmail.com>
2022-04-02 11:38:35 +0530

    BNaabm

---
+++ block-BNaabm/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.use("/", (req,res,next) => {
+  console.log(req.method,req.url)
+  next();
+})
+
+app.get('/',(req,res) => {
+  res.send('Welcome')
+});
+
+app.listen(4000,() => {
+  console.log('server is listing on port 4k')
+})
+
+
+
