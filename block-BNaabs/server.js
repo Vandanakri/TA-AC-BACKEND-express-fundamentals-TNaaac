6cc43660b25891842b3a38b254e6e0441054c803
vandana <vandana123annusah@gmail.com>
2022-04-09 16:20:42 +0530

    BNaabs

--- block-BNaabs/server.js
+++ block-BNaabs/server.js
@@ -5,37 +5,32 @@ var app = express();
 
 // middleware
 
-app.use(logger('dev'));
-app.use(cookieParser);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
 app.use(express.static(__dirname + '/public'));
+app.use(logger('dev'));
 
-// app.use((req,res,next)=> {
-//   console.log(req.cookie);
-//   next();
-// })
-
-
-// app.use('/about',(req,res,next) => {
-//   res.cookie("username", "suraj");
-//   res.end('About Page')
-// })
-
+//routes
 
 app.get('/', (req,res) => {
   res.sendFile(__dirname + "/index.html");
 })
 
+app.get('/new', (req,res) => {
+  res.sendFile(__dirname + "/new.html");
+})
 
-app.get('/users/:Vandana',(req,res) => {
-  var Vandana = req.params.Vandana;
-  res.send(Vandana);
+app.post('/new', (req,res) => {
+  res.json(req.body)
+})
+
+app.get('/users/:username',(req,res) => {
+  var username = req.params.username;
+  res.send(username);
 })
 
 
-
-app.listen(3000,()=> {
-  console.log('server is listining on port 3k')
+app.listen(5000,()=> {
+  console.log('server is listining on port 5k')
 })
