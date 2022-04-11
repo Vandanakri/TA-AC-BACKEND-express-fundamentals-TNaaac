dfa776278b26a32276fd1804b9c773ab7dd84a70
vandana <vandana123annusah@gmail.com>
2022-04-11 09:04:26 +0530

    BNaabw

---
+++ block-BNaabw/server.js
@@ -0,0 +1,34 @@
+//require
+var express = require('express');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+
+var app = express();
+
+//middleware
+app.use( express.json() );
+app.use( express.urlencoded( { extended : false  } ) );
+
+app.use(logger('dev'))
+app.use(cookieParser());
+
+//routes
+
+app.get('/', (req,res) => {
+  res.send('Welcome to Express')
+})
+
+app.get('/users', (req,res) => {
+  res.send('This is Assignment 2')
+})
+
+app.use(express.static(__dirname + '/assets'))
+
+app.use((err,req,res,next) => {
+  res.send(err);
+})
+
+
+app.listen(4000,() => {
+  console.log('server listening on port 4k')
+})
